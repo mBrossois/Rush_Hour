e1c1fe0e4d2f0324718c15302c0410195aaebf9a
mBrossois <mark.dnb@live.nl>
2014-04-01 12:27:02 +0200

    change

--- RushHour/src/RushHour/OpstartKlasse.java
+++ RushHour/src/RushHour/OpstartKlasse.java
@@ -1,14 +1,6 @@
 package RushHour;
 
-import javax.swing.JFrame;
-
-public class OpstartKlasse extends JFrame
+public class OpstartKlasse 
 {
-	public static void main(String [] args)
-	{
-		JFrame f = new JFrame();
-		f.setSize(100, 100);
-		f.setVisible(true);
-		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
-	}
+
 }
