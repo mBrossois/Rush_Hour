1d034bc97203d9e461fe5223856250c6289c760c
Mark de Neut Brossois <mark.dnb@live.nl>
2014-04-01 11:19:53 +0200

    OpstartKlasse toegevoegt
    JFrame aangemaakt

---
+++ RushHour/src/RushHour/OpstartKlasse.java
@@ -0,0 +1,14 @@
+package RushHour;
+
+import javax.swing.JFrame;
+
+public class OpstartKlasse extends JFrame
+{
+	public static void main(String [] args)
+	{
+		JFrame f = new JFrame();
+		f.setSize(100, 100);
+		f.setVisible(true);
+		f.setDefaultCloseOperation(f.EXIT_ON_CLOSE);
+	}
+}
